1a876398e0c168ceb1493d2d68a638d09a9925ca
gemafajar09 <gemafajarramadhan09@gmail.com>
2020-07-09 11:39:26 +0700

    issue2

--- home.php
+++ home.php
@@ -1,13 +1,97 @@
-<div class="row align-items-center">
-<div class="col-lg-6 order-lg-2">
-    <div class="p-5">
-    <img class="img-fluid rounded-circle" src="img/03.jpg" alt="">
+<header class="masthead text-center text-white">
+    <div class="masthead-content">
+        <div class="container">
+            <h1 class="masthead-heading mb-0">Politekni LP3i</h1>
+            <h2 class="masthead-subheading mb-0">Padang</h2>
+            <a href="#" class="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
+        </div>
     </div>
-</div>
-<div class="col-lg-6 order-lg-1">
-    <div class="p-5">
-    <h2 class="display-4">Let there be rock!</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+    <div class="bg-circle-1 bg-circle"></div>
+    <div class="bg-circle-2 bg-circle"></div>
+    <div class="bg-circle-3 bg-circle"></div>
+    <div class="bg-circle-4 bg-circle"></div>
+</header>
+
+<section style="background-color:white">
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="<?= $url ?>image/test.jpg" style="width:190px" class="rounded-circle"> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="<?= $url ?>image/test.jpg" style="width:190px" class="rounded-circle"> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="<?= $url ?>image/test.jpg" style="width:190px" class="rounded-circle"> 
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-</div>
+</section>
+
+<section style="background-color:white">
+    <div class="container-fluit py-3">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <center>
+                            <h3>Tentang Kami</h3>
+                        </center>
+                        <hr>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, est odit voluptate possimus eius eos sint necessitatibus vitae libero. Expedita, quas incidunt? In consectetur totam perspiciatis, animi ipsum sapiente omnis.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="studi" class="py-2">
+    <div class="card" style="background-color:transparent">
+        <div class="card-body">
+            <center><h5 style="color:white">Program Studi Politeknik LP3i Padang</h5></center>
+            <hr style="background-color:yelow">
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <img src="<?= $url ?>image/test.jpg" style="width:190px" class="rounded-circle"> 
+                </div>
+                <div class="col-md-4 text-center">
+                    <img src="<?= $url ?>image/test.jpg" style="width:190px" class="rounded-circle"> 
+                </div>
+                <div class="col-md-4 text-center">
+                    <img src="<?= $url ?>image/test.jpg" style="width:190px" class="rounded-circle"> 
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container-fluit py-2">
+        <div class="card">
+            <div class="card-body">
+                <div class="text-center">
+                    <h4>Testimoni</h4>
+                    <hr>
+                </div>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo itaque numquam iure ducimus, dolorem quos eaque perferendis placeat tempore culpa. Dolores incidunt laudantium eius maxime consequatur reiciendis illo fugiat ab?
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
