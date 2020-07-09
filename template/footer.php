493c3cc0d948dd7334d7bdd9c19287e60da59931
gemafajar09 <gemafajarramadhan09@gmail.com>
2020-07-09 21:46:47 +0700

    home

--- template/footer.php
+++ template/footer.php
@@ -4,8 +4,10 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.27641731232!2d100.3555788142542!3d-0.9443364993111194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2fd4b936fa4e3bb7%3A0xf45d477bf4a31d1a!2sLP3I%20Padang!5e0!3m2!1sid!2sid!4v1594273348637!5m2!1sid!2sid" width="600" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
           </div>
           <div class="col-md-6"></div>
-        </div>  
-        &copy; Politeknik LP3i Padang. By Memen Wahyudi, Gfr
+        </div>
+        <i>
+          &copy; Politeknik LP3i Padang. By Memen Wahyudi, Gfr
+        </i>  
     </footer>
   <script src="<?= $url ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
